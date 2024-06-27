4aecad9c08d38acb13484e3b9848be44137d76f5
Samuelhzx <lchbhh@163.com>
2024-06-27 21:37:45 +0800

    add post; add favicon

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1718901085',resources: [
+    cacheName: 'chirpy-1719495396',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
