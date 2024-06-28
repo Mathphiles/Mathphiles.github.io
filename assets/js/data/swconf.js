6e68ed9dbcbf75ea87e8cb4d46c59c3ce5ed8911
Samuelhzx <lchbhh@163.com>
2024-06-28 17:45:36 +0800

    optimize css

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1719495396',resources: [
+    cacheName: 'chirpy-1719567857',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
