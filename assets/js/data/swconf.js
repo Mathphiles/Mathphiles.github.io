bf4b353a9c986531efc0604290866b03b1208003
Samuelhzx <lchbhh@163.com>
2024-07-01 22:30:58 +0800

    add post; fix css

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1719567857',resources: [
+    cacheName: 'chirpy-1719844189',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
