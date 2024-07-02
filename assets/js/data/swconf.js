50af0d9126703f43910a98662d2004ba492569ab
Samuelhzx <lchbhh@163.com>
2024-07-02 12:52:47 +0800

    fix post

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1719844189',resources: [
+    cacheName: 'chirpy-1719895906',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
