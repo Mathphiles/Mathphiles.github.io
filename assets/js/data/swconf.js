db06f97ff8cc904cd01e6bacbff38bb3e0877915
Samuelhzx <115335868+Samuelhzx@users.noreply.github.com>
2025-07-05 23:09:14 +0800

    fix css

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1751727291',resources: [
+    cacheName: 'chirpy-1751728115',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
