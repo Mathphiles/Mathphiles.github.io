d277d0bef640bf4aaf75dd672bd18366770a4cbe
Samuelhzx <115335868+Samuelhzx@users.noreply.github.com>
2025-09-16 23:14:14 +0800

    add post

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755684399',resources: [
+    cacheName: 'chirpy-1758035612',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
